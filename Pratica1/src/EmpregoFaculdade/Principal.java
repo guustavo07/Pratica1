5208cdba1b894f3b0efdaf1c65bdf2f63bddb9dc
Gustavo Henrique <84508964+guustavo07@users.noreply.github.com>
2022-03-15 22:05:00 -0300

    Add files via upload

---
+++ Pratica1/src/EmpregoFaculdade/Principal.java
@@ -0,0 +1,12 @@
+package questao2;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		EmpregadoDaFaculdade e1 = new EmpregadoDaFaculdade("Rosa", 1200.00, 10);
+		
+		System.out.println(e1.getGastos());
+		System.out.println(e1.getInfo());
+	}
+
+}
